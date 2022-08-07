914261cd04d6361b4beb107a0f5e90dc0165f8d1
kstearn <101226667+kstearn@users.noreply.github.com>
2022-08-07 14:52:09 -0400

    Add Ship class

---
+++ Ship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BattleshipGame
+{
+    public class Ship
+    {
+        public int size;
+        public char letter;
+
+        public Ship(int size, char letter)
+        {
+            this.size = size;
+            this.letter = letter;
+        }
+    }
+}
+
